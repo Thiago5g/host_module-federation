d4f3d6e6bd9ec3460ee47876a87604c93b600f05
thiago.martins <thiago.martins@extreme.digital>
2025-11-07 00:36:57 -0300

    fix url env

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,19 @@ import federation from '@originjs/vite-plugin-federation'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
+  const remoteUrl = env.VITE_REMOTE_APP_URL || 'http://localhost:5001'
+  const remoteEntry = `${remoteUrl}/assets/remoteEntry.js`
+  
+  console.log('🔍 VITE_REMOTE_APP_URL:', remoteUrl)
+  console.log('🌐 Remote URL completa:', remoteEntry)
+
   return {
     plugins: [
       react(),
       federation({
         name: 'host',
         remotes: {
-          remote: `${env.REMOTE_APP_URL || 'https://remote-module-federation.vercel.app/'}/assets/remoteEntry.js`
+          remote: remoteEntry
         },
         shared: ['react', 'react-dom']
       })
